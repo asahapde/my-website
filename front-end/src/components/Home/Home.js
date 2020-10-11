80c6561b2d5b523dadcea40a9059e90487918da0
asahapde <60831459+asahapde@users.noreply.github.com>
2020-10-11 00:27:27 -0400

    Completed Home

--- front-end/src/components/Home/Home.js
+++ front-end/src/components/Home/Home.js
@@ -8,10 +8,10 @@ class Home extends Component {
     render() {
         return (
             <Container>
-                <Image src={image} thumbnail className="m-5"/>
-                <h3 className="m-3">Software Engineering student at Univeristy of Western Ontario</h3>
-                <h4 className="m-3">Seeking for Summer 2021 Internships</h4>
-                <h4 className="m-3">Available for 4-16 months</h4>
+                <Image src={image} thumbnail className="m-5 image"/>
+                <h3 className="m-3"><strong>Software Engineering</strong> student at <strong className="uwo">University of Western Ontario</strong></h3>
+                <h4 className="m-3">Seeking Summer 2021 Internships</h4>
+                <h5 className="m-3"><a href="https://drive.google.com/file/d/1sb80t1MkVx8ZQIFECEAOwEGCxQDSb7HF/view?usp=sharing" target="_blank" className="resume"><strong>RESUME</strong></a></h5>
                 <Row className="m-5">
                     <Col><a href="https://www.linkedin.com/in/abdullah-sahapdeen/" target="_blank"><FaLinkedin size="5em" className="iconLinked"/></a></Col>
                     <Col><a href="mailto:asahapde@gmail.com" target="_blank"><FaEnvelopeOpen size="5em" className="iconMail"/></a></Col>
